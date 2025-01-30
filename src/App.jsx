826db810ace4ca3913c7403a2b07805c26c889e1
Ashhish-h <kushwaha.ashish2611@gmail.com>
2025-01-30 22:23:09 +0530

    create: signup page and add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 
 import LoginPage from './pages/LoginPage'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router'
+import SignUpPage from './pages/SignUpPage'
 
 function App() {
 
 
   return (
-    <>
-      <LoginPage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </Router>
   )
 }
 
