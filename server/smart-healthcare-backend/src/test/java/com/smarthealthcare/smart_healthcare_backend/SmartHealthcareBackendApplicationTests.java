2e6f70741b949243e655d371e5ac4fd0f04489be
Ashhish-h <kushwaha.ashish2611@gmail.com>
2025-02-12 18:21:47 +0530

    add: smart-healthcare-backend for backend part of the project

---
+++ server/smart-healthcare-backend/src/test/java/com/smarthealthcare/smart_healthcare_backend/SmartHealthcareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthealthcare.smart_healthcare_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHealthcareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
