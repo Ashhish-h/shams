678f76d628fb5d21c28bb3d612025dca21d94ca5
Ashhish-h <kushwaha.ashish2611@gmail.com>
2025-03-12 15:31:26 +0530

    add: create UserRegistrationDTO for user registration data handling

---
+++ server/smart-healthcare-backend/src/main/java/com/healthcarebackend/smart_healthcare_backend/dto/UserRegistrationDTO.java
@@ -0,0 +1,28 @@
+package com.healthcarebackend.smart_healthcare_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegistrationDTO {
+
+    private String name;
+    private String email;
+    private String password;
+    private String role; // Either "patient" or "doctor"
+    private String mobileNo;
+
+    // Patient-specific fields
+    private Integer age; // Patient's age
+
+    // Doctor-specific fields
+    private Integer experience; // Doctor's experience
+    private String clinic; // Doctor's associated clinic
+
+    
+}
