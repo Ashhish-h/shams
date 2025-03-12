5eba5d67def4a96b7ccbebe06579876180dd1462
Ashhish-h <kushwaha.ashish2611@gmail.com>
2025-03-12 13:07:46 +0530

    add: initial project setup for smart-healthcare-backend with Maven configuration and basic application structure

---
+++ server/smart-healthcare-backend/src/main/java/com/healthcarebackend/smart_healthcare_backend/SmartHealthcareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.healthcarebackend.smart_healthcare_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHealthcareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHealthcareBackendApplication.class, args);
+	}
+
+}
---
+++ server/smart-healthcare-backend/src/test/java/com/healthcarebackend/smart_healthcare_backend/SmartHealthcareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcarebackend.smart_healthcare_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHealthcareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
